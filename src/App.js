0a5ff3da47a2157618e179c0d84df882d6c66455
kevin a <karias@akurey.com>
2021-12-24 13:33:33 -0600

    started page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import Header from "./components/Header";
-import Button from "./atoms/Button";
 function App() {
   return (
     <div className="App">
       <Header />
-      <div>
-        <Button text="Hola" />
-        <Button type="secondary" text="Wenas" />
-        <Button type="danger" text="Adió" />
-      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
+import Button from "../atoms/Button";
 
 import PropTypes from "prop-types";
 
 export default function Header() {
   return (
-    <div className="header">
-      <h1>
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter
-              .typeString("I'm Kevin Arias Chinchilla.")
-              .pauseFor(1500)
-              .deleteChars(1)
-              .typeString(",")
-              .start();
-          }}
-        />
-      </h1>
-    </div>
+    <header className="header">
+      <svg className="header__logo" />
+      <div className="header__buttons">
+        <Button text="Contact me" />
+      </div>
+    </header>
   );
 }
